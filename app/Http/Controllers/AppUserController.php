b7d35e775c49bb1ec70f90a5a4ed4b06e78f3f3e
Andreas Siegwarth <andreassiegwarth@startmail.com>
2025-05-28 20:50:44 +0200

    * add: /register endpoint

--- app/Http/Controllers/AppUserController.php
+++ app/Http/Controllers/AppUserController.php
@@ -4,17 +4,28 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class AppUserController extends Controller
 {
     public function findAll() {
-
         $selectStatement = ltrim(<<<'SQL'
             SELECT * FROM app_user;
         SQL);
-
         $appUsers = DB::select($selectStatement, []);
-
         return $appUsers;
+    }
+
+    public function register(Request $request) {
+        $hashedPassword = Hash::make($request["password"]);
+        $insertStatement = ltrim(<<<'SQL'
+            INSERT INTO app_user (username, email, password) values (?, ?, ?)
+        SQL);
+        $newUser = DB::insert($insertStatement, [
+            $request["username"],
+            $request["email"],
+            $hashedPassword,
+        ]);
+        return $newUser;
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('/appuser', [AppUserController::class, 'findAll']);
+
+Route::post('/register', [AppUserController::class, 'register']);
