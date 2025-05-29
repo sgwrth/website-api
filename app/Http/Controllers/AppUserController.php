c97f47d00cce9934f57a53673e1a3e9fdee31d27
Andreas Siegwarth <andreassiegwarth@startmail.com>
2025-05-29 05:55:42 +0200

    * add: check for already existing email in db when trying to create new   user

--- app/Http/Controllers/AppUserController.php
+++ app/Http/Controllers/AppUserController.php
@@ -12,20 +12,34 @@ class AppUserController extends Controller
         $selectStatement = ltrim(<<<'SQL'
             SELECT * FROM app_user;
         SQL);
-        $appUsers = DB::select($selectStatement, []);
-        return $appUsers;
+        return DB::select($selectStatement, []);
+    }
+
+    public function usersFound($email) {
+        $selectStatement = ltrim(<<<'SQL'
+            SELECT COUNT(*) FROM app_user WHERE email = ?
+        SQL);
+        return DB::scalar($selectStatement, [$email]);
     }
 
     public function register(Request $request) {
+        $usersFound = $this->usersFound($request["email"]);
+        if ($usersFound > 0) {
+            return response()->json(['message' => 'error: email already exists']);
+        }
         $hashedPassword = Hash::make($request["password"]);
         $insertStatement = ltrim(<<<'SQL'
-            INSERT INTO app_user (username, email, password) values (?, ?, ?)
+            INSERT INTO app_user (username, email, password) values (:username, :email, :password)
         SQL);
-        $newUser = DB::insert($insertStatement, [
-            $request["username"],
-            $request["email"],
-            $hashedPassword,
+        $success = DB::insert($insertStatement, [
+            'username' => $request["username"],
+            'email' => $request["email"],
+            'password' => $hashedPassword,
         ]);
-        return $newUser;
+        if ($success) {
+            return response()->json(['message' => 'user created!'], 201);
+        } else {
+            return response()->json(['message' => 'something went wrong ...'], 500);
+        }
     }
 }
